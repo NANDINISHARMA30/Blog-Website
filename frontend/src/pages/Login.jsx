7285adf6d26134c137c619961043cbebf6788bf7
Nandini Sharma <144807003+NANDINISHARMA30@users.noreply.github.com>
2026-02-20 23:20:40 +0530

    Login  Page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,39 @@
+import { useState, useContext } from "react";
+import API from "../api/axios";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await API.post("/auth/login", {
+      email,
+      password,
+    });
+    login(data);
+    navigate("/");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
